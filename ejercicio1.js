39d02a91275ec69ed101bd90a897b902a4102deb
thaliavynes1 <casamarketthalia@email.com>
2026-06-15 11:33:43 -0500

    segundo ejercico

--- ejercicio1.js
+++ ejercicio1.js
@@ -3,8 +3,21 @@
 let  cuentaA = 5; 
 let cuentaB = 10; 
 
-a = a + b 
-b = a - b
-a = a - b 
+cuentaA = cuentaA + cuentaB 
+cuentaB = cuentaA - cuentaB
+cuentaA = cuentaA - cuentaB
 
-console.log(cuentaA + cuentaB)
+console.log("A:", cuentaA);
+console.log("B:", cuentaB);
+
+// ejercicio 2
+
+let  nivel = Number(prompt("ingrese el nivel"))
+let honor = 100;
+let tienePaseVIP = false;
+
+if (nivel >= 20 && (honor === 100 || tienePaseVIP === true)) {
+  console.log("Puede entrar al servidor");
+} else {
+  console.log("No puede entrar al servidor");
+}
